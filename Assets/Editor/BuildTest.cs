438e046241e851bd7a5b024c9a07743f5dac4472
leehyewon <hwlee@moveint.io>
2024-11-11 18:52:00 +0900

    test

--- Assets/Editor/BuildTest.cs
+++ Assets/Editor/BuildTest.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 class BuildTest
 {
@@ -11,7 +12,6 @@ class BuildTest
     [MenuItem("Build/Test")]
     static void PerformBuild()
     {
-
         BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/MyGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
     }
     private static string[] FindEnabledEditorScenes()
@@ -23,5 +23,24 @@ class BuildTest
             EditorScenes.Add(scene.path);
         }
         return EditorScenes.ToArray();
+    }
+
+    [MenuItem("Build/BundleBuildTest")]
+    public void BuildAllAssetBundles()
+    {
+        string path = Application.dataPath + "/AssetBundles";
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+    public void BuildNeedAssetBundle(string bundleName)
+    {
+        if (!Directory.Exists(Application.dataPath + "/AssetBundles")) 
+        {
+            Directory.CreateDirectory(Application.dataPath + "/AssetBundles"); 
+        }
+        AssetBundleBuild[] buildBundles = new AssetBundleBuild[1];
+        buildBundles[0].assetBundleName = bundleName; 
+        buildBundles[0].assetNames = AssetDatabase.GetAssetPathsFromAssetBundle(bundleName);
+        BuildPipeline.BuildAssetBundles(Application.dataPath + "/AssetBundles", buildBundles, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
     }
 }
---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundle
+{
+    [UnityEditor.MenuItem("Assets/Build All Asset Bundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDir = "Assets/StreamingAssets";
+
+        if(!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(assetBundleDir);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDir, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
